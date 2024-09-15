6199b8f3df21610d61117b8d0cbf1981d717f7e8
Adam Jones <domdomegg+git@gmail.com>
2024-09-15 21:21:38 +0100

    Enable zip and html uploads

---
+++ src/DragDropFile.tsx
@@ -0,0 +1,73 @@
+import React, { useState, useEffect, useCallback } from 'react';
+
+const DragDropFile: React.FC<React.PropsWithChildren<{
+  handleFiles: (files: FileList) => void,
+}>> = ({ children, handleFiles }) => {
+  const [drag, setDrag] = useState(false);
+  let dragCounter = 0;
+
+  const handleDrag = useCallback((e) => {
+    e.preventDefault();
+    e.stopPropagation();
+  }, []);
+
+  const handleDragIn = useCallback((e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    dragCounter++;
+    if (e.dataTransfer.items && e.dataTransfer.items.length > 0) {
+      setDrag(true);
+    }
+  }, []);
+
+  const handleDragOut = useCallback((e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    dragCounter--;
+    if (dragCounter === 0) {
+      setDrag(false);
+    }
+  }, []);
+
+  const handleDrop = useCallback((e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setDrag(false);
+    if (e.dataTransfer.files && e.dataTransfer.files.length > 0) {
+      handleFiles(e.dataTransfer.files);
+      e.dataTransfer.clearData();
+      dragCounter = 0;
+    }
+  }, [handleFiles]);
+
+  useEffect(() => {
+    const el = document.body;
+    el.addEventListener('dragenter', handleDragIn);
+    el.addEventListener('dragleave', handleDragOut);
+    el.addEventListener('dragover', handleDrag);
+    el.addEventListener('drop', handleDrop);
+
+    return () => {
+      el.removeEventListener('dragenter', handleDragIn);
+      el.removeEventListener('dragleave', handleDragOut);
+      el.removeEventListener('dragover', handleDrag);
+      el.removeEventListener('drop', handleDrop);
+    };
+  }, [handleDrag, handleDragIn, handleDragOut, handleDrop]);
+
+  return (
+    <div className="file-area">
+      {drag && (<>
+        <div className="file-area-shadow" />
+        <div className="file-area-inner">
+          <h1>
+            Drop your .zip or .html here
+          </h1>
+        </div>
+      </>)}
+      {children}
+    </div>
+  );
+};
+
+export default DragDropFile;
